34f9ef876d733c15ce8cbc33434b337faa66ae42
cataramocini-stack <justinianoconstantinogrande@gmail.com>
2026-03-05 16:32:35 -0300

    Update osint_monitor.py

--- osint_monitor.py
+++ osint_monitor.py
@@ -63,13 +63,12 @@ logger = logging.getLogger(__name__)
 
 def get_config():
 
-    config = SHConfig()
+    config = SHConfig(profile=None)
 
     config.sh_client_id = os.getenv("CDSE_CLIENT_ID")
     config.sh_client_secret = os.getenv("CDSE_CLIENT_SECRET")
 
     config.sh_base_url = "https://sh.dataspace.copernicus.eu"
-
     config.sh_token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
 
     return config
